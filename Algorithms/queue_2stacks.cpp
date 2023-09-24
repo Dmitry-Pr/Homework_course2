837171c63dffc11ec4143ba08b67ab9da2ec7dd6
Dmitry-Pr <61019019+Dmitry-Pr@users.noreply.github.com>
2023-09-24 19:43:24 +0300

    Update queue_2stacks.cpp
    Changed Min and Back, now both are much faster

--- Algorithms/queue_2stacks.cpp
+++ Algorithms/queue_2stacks.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <stack>
+#include <utility>
 
 template<class T>
 class MyQueue {
 private:
-    std::stack<T> first_;
-    std::stack<T> second_;
+    std::stack<std::pair<T, T>> first_;
+    std::stack<std::pair<T, T>> second_;
     int32_t size_;
+    int32_t last_;
 public:
     MyQueue() {
         size_ = 0;
+        last_ = 0;
     }
 
     void Push(T val) {
-        first_.push(val);
+        if (first_.empty()) {
+            first_.push({val, val});
+        } else {
+            first_.push({val, std::min(val, first_.top().second)});
+        }
+        last_ = val;
         ++size_;
     }
 
@@ -31,72 +39,58 @@ public:
         }
         if (!second_.empty()) {
             second_.pop();
-            --size_;
         } else {
+            second_.push({first_.top().first, first_.top().first});
+            first_.pop();
             while (!first_.empty()) {
-                second_.push(first_.top());
+                second_.push({first_.top().first, std::min(first_.top().first, second_.top().second)});
                 first_.pop();
             }
-            if (!second_.empty()) {
-                second_.pop();
-                --size_;
-            }
+            second_.pop();
         }
+        --size_;
     }
 
     T Front() {
         if (Empty()) {
             throw std::out_of_range("Очередь пустая");
         }
-        if (!second_.empty()) {
-            return second_.top();
-        } else {
+        if (second_.empty()) {
+            second_.push({first_.top().first, first_.top().first});
+            first_.pop();
             while (!first_.empty()) {
-                second_.push(first_.top());
+                second_.push({first_.top().first, std::min(first_.top().first, second_.top().second)});
                 first_.pop();
             }
-            return second_.top();
         }
+        return second_.top().first;
     }
 
     T Back() {
         if (Empty()) {
             throw std::out_of_range("Очередь пустая");
         }
-        if (!first_.empty()) {
-            return first_.top();
-        } else {
-            while (!second_.empty()) {
-                first_.push(second_.top());
-                second_.pop();
-            }
-            return first_.top();
-        }
+        return last_;
     }
 
     void Clear() {
         size_ = 0;
-        first_ = std::stack<T>();
-        second_ = std::stack<T>();
+        first_ = std::stack<std::pair<T, T>>();
+        second_ = std::stack<std::pair<T, T>>();
     }
 
     T Min() {
         if (Empty()) {
             throw std::out_of_range("Очередь пуста");
         }
-        while (!second_.empty()) {
-            first_.push(second_.top());
-            second_.pop();
-        }
-        T min = first_.top();
-        while (!first_.empty()) {
-            if (min > first_.top()) {
-                min = first_.top();
+        if (!first_.empty()) {
+            if (!second_.empty()) {
+                return std::min(first_.top().second, second_.top().second);
+            } else {
+                return first_.top().second;
             }
-            second_.push(first_.top());
-            first_.pop();
         }
-        return min;
+        return second_.top().second;
     }
 };
 
@@ -106,9 +100,9 @@ int main() {
         q.Push(1);
         q.Push(2);
         q.Push(3);
-        std::cout << q.Front() << ' ' << q.Back() << '\n';
+        std::cout << q.Front() << ' ' << q.Back() << ' ' << q.Min() << '\n';
         q.Pop();
-        std::cout << q.Front() << ' ' << q.Back() << '\n';
+        std::cout << q.Front() << ' ' << q.Back() << ' ' << q.Min() << '\n';
         q.Push(4);
         std::cout << q.Front() << ' ' << q.Back() << '\n';
         std::cout << q.Min();
