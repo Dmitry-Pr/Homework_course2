7db014aa0aafa4855348d15d34462582b06169fc
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-02-24 10:00:57 +0300

    Upgraded HW1 Android Added switching application themes

--- Android/HW1/app/src/main/java/com/example/hw1/MainActivity.java
+++ Android/HW1/app/src/main/java/com/example/hw1/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.hw1;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+    private SwitchCompat theme_switcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        theme_switcher = findViewById(R.id.theme_switcher);
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're in day light mode
+                theme_switcher.setChecked(false);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're in dark mode
+                theme_switcher.setChecked(true);
+                break;
+        }
+        theme_switcher.setOnClickListener(v -> {
+            boolean checked = theme_switcher.isChecked();
+
+            if (checked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
     }
 }
