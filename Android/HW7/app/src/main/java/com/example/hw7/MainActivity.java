4f19bc8cb1cae57f9578c59b23f47aa33b8172d8
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-22 11:09:41 +0300

    Completed HW7 Android

--- Android/HW7/app/src/main/java/com/example/hw7/MainActivity.java
+++ Android/HW7/app/src/main/java/com/example/hw7/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,22 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         theme_switcher = findViewById(R.id.theme_switcher);
         int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        SharedPreferences sharedPreferences = getSharedPreferences("ThemePref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         switch (currentNightMode) {
             case Configuration.UI_MODE_NIGHT_NO:
                 // Night mode is not active, we're in day light mode
                 theme_switcher.setChecked(false);
+                editor.putBoolean("isDarkTheme", false);
                 break;
             case Configuration.UI_MODE_NIGHT_YES:
                 // Night mode is active, we're in dark mode
                 theme_switcher.setChecked(true);
+                editor.putBoolean("isDarkTheme", true);
                 break;
         }
         theme_switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            if (buttonView.isPressed()) {
+                if (!isChangingConfigurations()) {
+                    if (isChecked) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        editor.putBoolean("isDarkTheme", true);
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        editor.putBoolean("isDarkTheme", false);
+                    }
+                    editor.apply();
+                }
             }
         });
+        Button button = findViewById(R.id.maps_button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
--- Android/HW7/app/src/main/java/com/example/hw7/MapsActivity.java
+++ Android/HW7/app/src/main/java/com/example/hw7/MapsActivity.java
@@ -1,33 +1,69 @@
 package com.example.hw7;
 
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.example.hw7.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String MAP_FRAGMENT_TAG = "map_fragment";
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = getSharedPreferences("ThemePref", MODE_PRIVATE);
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        SupportMapFragment mapFragment;
+        if (savedInstanceState == null) {
+            // First time running, create a new map fragment
+            mapFragment = SupportMapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.map, mapFragment, MAP_FRAGMENT_TAG)
+                    .commit();
+        } else {
+            // Activity is being recreated, retrieve the existing map fragment
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag(MAP_FRAGMENT_TAG);
+        }
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.invalidate();
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MapsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 
     /**
@@ -42,10 +78,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, location -> {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker in Current Location"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                        } else {
+                            // Add a marker in Sydney and move the camera
+                            LatLng sydney = new LatLng(-34, 151);
+                            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                        }
+                    });
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Set the map style
+        SharedPreferences sharedPreferences = getSharedPreferences("ThemePref", MODE_PRIVATE);
+        boolean isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false);
+        if (isDarkTheme) {
+            try {
+                googleMap.setMapStyle(
+                        MapStyleOptions.loadRawResourceStyle(
+                                this, R.raw.mapstyle_night));
+
+            } catch (Resources.NotFoundException e) {
+                Log.e("MapsActivity", "Can't find style. Error: ", e);
+            }
+        }
     }
 }
