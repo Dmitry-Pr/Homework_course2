28755948904ae2bb898348c5b75aa3aef70dc5bf
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-24 22:17:10 +0300

    Completed HW11 Android

---
+++ Android/HW11/app/src/main/java/com/example/hw11/MainActivity.java
@@ -0,0 +1,174 @@
+package com.example.hw11;
+
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    private SwitchCompat theme_switcher;
+    private static final int PERMISSION_REQUEST_CODE = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boolean launchedFromNotification = getIntent().getBooleanExtra("launchedFromNotification", false);
+        if (launchedFromNotification) {
+            AlertDialog.Builder dialog = new
+                    AlertDialog.Builder(MainActivity.this);
+            dialog.setMessage("Спасибо, что вспомнили обо мне!");
+            dialog.setTitle("Уведомление");
+            dialog.setNeutralButton("OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.setIcon(android.R.drawable.ic_dialog_info);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+        }
+        theme_switcher = findViewById(R.id.theme_switcher);
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're in day light mode
+                theme_switcher.setChecked(false);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're in dark mode
+                theme_switcher.setChecked(true);
+                break;
+        }
+        theme_switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            requestMultiplePermissions();
+            Context context = MainActivity.this;
+            NotificationChannel newnotchan = null;
+            if (android.os.Build.VERSION.SDK_INT >=
+                    android.os.Build.VERSION_CODES.O) {
+                newnotchan = new
+                        NotificationChannel("mychannel1", "mychannel", NotificationManager
+                        .IMPORTANCE_HIGH);
+                AudioAttributes audioAttributes = new
+                        AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .build();
+
+                newnotchan.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/" + R.raw.al_heylisten),
+                        audioAttributes);
+            }
+            NotificationManager notificationManager = (NotificationManager)
+                    getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationManager.createNotificationChannel(newnotchan);
+            }
+            Notification notification = null;
+            if (android.os.Build.VERSION.SDK_INT >=
+                    android.os.Build.VERSION_CODES.O) {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("launchedFromNotification", true);
+                PendingIntent pendingIntent = PendingIntent.getActivity(
+                        this,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                );
+                Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+                notification = new
+                        Notification.Builder(context, "mychannel1")
+                        .setContentTitle("Hi from HW11")
+                        .setContentText("Hey, listen!")
+                        .setTicker("new notification!")
+                        .setChannelId("mychannel1")
+                        .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                        .setOngoing(true)
+                        .setSmallIcon(R.drawable.logo)
+                        .setLargeIcon(largeIcon)
+                        .setContentIntent(pendingIntent)
+                        .build();
+            }
+            notificationManager.notify(0, notification);
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.about) {
+            AlertDialog.Builder dialog = new
+                    AlertDialog.Builder(MainActivity.this);
+            try {
+                dialog.setMessage(getTitle().toString() + " версия " +
+                        getPackageManager().getPackageInfo(getPackageName(), 0).versionName +
+                        "\r\n\nПрограмма со звуком \r\n\n Автор - " +
+                        getString(R.string.person));
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            dialog.setTitle("О программе");
+            dialog.setNeutralButton("OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.setIcon(R.mipmap.ic_launcher_round);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void requestMultiplePermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        POST_NOTIFICATIONS
+                },
+                PERMISSION_REQUEST_CODE);
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[]
+            permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return;
+            } else finish();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
