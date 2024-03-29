79050f31db5a63b8bf1dfbf275d891659d3d43c4
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-29 14:54:07 +0300

    Completed HW14

---
+++ Android/HW14/app/src/main/java/com/example/hw14/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.hw14;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private SwitchCompat theme_switcher;
+    private ImageView imageView;
+    private ActivityResultLauncher<Intent> mActivityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        theme_switcher = findViewById(R.id.theme_switcher);
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're in day light mode
+                theme_switcher.setChecked(false);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're in dark mode
+                theme_switcher.setChecked(true);
+                break;
+        }
+        theme_switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isPressed()) {
+                if (isChecked) {
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+
+        mActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Обрабатываем результат
+                        Intent data = result.getData();
+                        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
+        );
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mActivityResultLauncher.launch(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.about) {
+            AlertDialog.Builder dialog = new
+                    AlertDialog.Builder(MainActivity.this);
+            try {
+                dialog.setMessage(getTitle().toString() + " версия " +
+                        getPackageManager().getPackageInfo(getPackageName(), 0).versionName +
+                        "\r\n\nПрограмма с фото \r\n\n Автор - " +
+                        getString(R.string.person));
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            dialog.setTitle("О программе");
+            dialog.setNeutralButton("OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.setIcon(R.mipmap.ic_launcher_round);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
