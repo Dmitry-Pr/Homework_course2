4e89d2fdf6fef5f92cbc20ff98c54649a6e9377a
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-30 12:12:43 +0300

    First working version of Project

---
+++ Android/Project/app/src/main/java/com/example/antiprocrastination/MainActivity.java
@@ -0,0 +1,223 @@
+package com.example.antiprocrastination;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private SwitchCompat theme_switcher;
+    private TextView lastSessionText;
+    private Boolean isServiceRunning = false;
+    private TextView recordText;
+    private BroadcastReceiver receiver;
+    private SharedPreferences sharedPreferences;
+    private ProgressBar recordProgressBar;
+    private CountDownTimer countDownTimer;
+    private long recordTimeInSeconds;
+    private long millisUntilFinished;
+    private TextView timerText;
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        theme_switcher = findViewById(R.id.theme_switcher);
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                // Night mode is not active, we're in day light mode
+                theme_switcher.setChecked(false);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                // Night mode is active, we're in dark mode
+                theme_switcher.setChecked(true);
+                break;
+        }
+        theme_switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isPressed()) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("progress", recordProgressBar.getProgress());
+                if (countDownTimer != null) {
+                    editor.putLong("remainingTime", millisUntilFinished);
+                }
+                editor.putBoolean("isServiceRunning", isServiceRunning);
+                editor.apply();
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+        lastSessionText = findViewById(R.id.last_session_text);
+        recordText = findViewById(R.id.record_text);
+        sharedPreferences = getSharedPreferences("appPreferences", MODE_PRIVATE);
+        long recordTime = sharedPreferences.getLong("recordTime", 0);
+        isServiceRunning = sharedPreferences.getBoolean("isServiceRunning", false);
+        recordText.setText(formatTime(recordTime));
+        lastSessionText.setText(formatTime(0));
+        recordProgressBar = findViewById(R.id.recordProgressBar);
+        timerText = findViewById(R.id.timerText);
+        if (isServiceRunning) {
+            int progress = sharedPreferences.getInt("progress", 0);
+            recordProgressBar.setProgress(progress);
+            long remainingTime = sharedPreferences.getLong("remainingTime", 0);
+            if (remainingTime > 0) {
+                startCountDownTimer(TimeUnit.MILLISECONDS.toSeconds(remainingTime));
+            }
+        }
+        recordTimeInSeconds = recordTime;
+        MainActivity obj = this;
+        SwitchCompat buttonStart = findViewById(R.id.button_start);
+        buttonStart.setOnCheckedChangeListener((view, isChecked) -> {
+            if (view.isPressed()) {
+                if (isChecked) {
+                    isServiceRunning = true;
+
+                    Intent intent = new Intent(obj, SensorService.class);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        startForegroundService(intent);
+
+                    } else {
+                        startService(intent);
+                    }
+                    startCountDownTimer(recordTimeInSeconds);
+                } else {
+                    isServiceRunning = false;
+                    Intent intent = new Intent(obj, SensorService.class);
+                    stopService(intent);
+                    if (countDownTimer != null) {
+                        countDownTimer.cancel();
+                    }
+                    recordProgressBar.setProgress(0);
+                }
+            }
+        });
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String stationaryTime = intent.getStringExtra("stationaryTime");
+                lastSessionText.setText(stationaryTime);
+                String recordTime = intent.getStringExtra("recordTime"); // Получаем recordTime из intent
+                recordText.setText(recordTime); // Обновляем recordText
+                recordTimeInSeconds = TimeUnit.HOURS.toSeconds(Integer.parseInt(recordTime.split(":")[0]))
+                        + TimeUnit.MINUTES.toSeconds(Integer.parseInt(recordTime.split(":")[1]))
+                        + Integer.parseInt(recordTime.split(":")[2]);
+                if (isServiceRunning) {
+                    startCountDownTimer(recordTimeInSeconds);
+                }
+            }
+        };
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            registerReceiver(receiver, new IntentFilter("com.example.antiprocrastination.UPDATE_TIME"), Context.RECEIVER_EXPORTED);
+//        }
+
+
+    }
+
+    private void startCountDownTimer(long recordTimeInSeconds) {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        countDownTimer = new CountDownTimer(recordTimeInSeconds * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                MainActivity.this.millisUntilFinished = millisUntilFinished;
+                int progress = (int) ((1 - (double) millisUntilFinished / (recordTimeInSeconds * 1000)) * 100);
+                recordProgressBar.setProgress(progress);
+            }
+
+            @Override
+            public void onFinish() {
+                recordProgressBar.setProgress(100);
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter("com.example.antiprocrastination.UPDATE_TIME"), Context.RECEIVER_EXPORTED);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.about) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            try {
+                dialog.setMessage(getTitle().toString() + " версия " +
+                        getPackageManager().getPackageInfo(getPackageName(), 0).versionName +
+                        "\r\n\nПроект для борьбы с прокрастинацией \r\n\n Автор - " +
+                        getString(R.string.person));
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+            dialog.setTitle("О программе");
+            dialog.setNeutralButton("OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.setIcon(R.mipmap.ic_launcher_round);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private String formatTime(long timeInSeconds) {
+        long hours = TimeUnit.SECONDS.toHours(timeInSeconds);
+        long minutes = TimeUnit.SECONDS.toMinutes(timeInSeconds) % 60;
+        long seconds = timeInSeconds % 60;
+        return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+    }
+}
---
+++ Android/Project/app/src/main/java/com/example/antiprocrastination/SensorService.java
@@ -0,0 +1,193 @@
+package com.example.antiprocrastination;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class SensorService extends Service implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Vibrator vibrator;
+    private static final String CHANNEL_ID = "SensorServiceChannel";
+    private long lastMovementTime;
+    private long stationaryTime;
+    private long sessionRecord;
+    private long recordTime;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+            Log.e("SensorService", "Device does not support accelerometer");
+        }
+        sharedPreferences = getSharedPreferences("appPreferences", MODE_PRIVATE);
+        stationaryTime = sharedPreferences.getLong("stationaryTime", 0);
+        recordTime = sharedPreferences.getLong("recordTime", 0);
+        lastMovementTime = System.currentTimeMillis();
+        sessionRecord = 0;
+    }
+
+    @SuppressLint("ForegroundServiceType")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Anti Procrastination Service")
+                .setContentText("Service is running...")
+                .setSmallIcon(R.drawable.ic_sun)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1, notification);
+
+        return START_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Anti Procrastination Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stationaryTime = System.currentTimeMillis() - lastMovementTime;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        long stationaryTimeInSeconds = TimeUnit.MILLISECONDS.toSeconds(stationaryTime);
+        editor.putLong("last_session_time", stationaryTimeInSeconds);
+        Boolean flag1 = false;
+        Boolean flag2 = false;
+        if (stationaryTimeInSeconds > recordTime) {
+            recordTime = stationaryTimeInSeconds;
+            editor.putLong("recordTime", recordTime);
+            flag1 = true;
+        }
+        if (stationaryTimeInSeconds > sessionRecord) {
+            sessionRecord = stationaryTimeInSeconds;
+            flag2 = true;
+        }
+        editor.apply();
+        if (flag1 || flag2) {
+            Intent intent = new Intent("com.example.antiprocrastination.UPDATE_TIME");
+            intent.putExtra("stationaryTime", formatTime(stationaryTimeInSeconds));
+            intent.putExtra("recordTime", formatTime(recordTime));
+            sendBroadcast(intent);
+        }
+
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            if (z < 9 && z > -9) {
+                if (vibrator.hasVibrator()) {
+                    vibrator.vibrate(10000);
+                }
+                if (lastMovementTime != 0) {
+                    stationaryTime = System.currentTimeMillis() - lastMovementTime;
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    long stationaryTimeInSeconds = TimeUnit.MILLISECONDS.toSeconds(stationaryTime);
+                    editor.putLong("last_session_time", stationaryTimeInSeconds);
+                    Boolean flag1 = false;
+                    Boolean flag2 = false;
+                    if (stationaryTimeInSeconds > recordTime) {
+                        recordTime = stationaryTimeInSeconds;
+                        editor.putLong("recordTime", recordTime);
+                        flag1 = true;
+                    }
+                    if (stationaryTimeInSeconds > sessionRecord) {
+                        sessionRecord = stationaryTimeInSeconds;
+                        flag2 = true;
+                    }
+                    editor.apply();
+                    if (flag1 || flag2) {
+                        Intent intent = new Intent("com.example.antiprocrastination.UPDATE_TIME");
+                        intent.putExtra("stationaryTime", formatTime(stationaryTimeInSeconds));
+                        intent.putExtra("recordTime", formatTime(recordTime));
+                        sendBroadcast(intent);
+                    }
+                }
+                lastMovementTime = System.currentTimeMillis();
+            } else {
+                vibrator.cancel();
+            }
+        }
+    }
+
+    private String formatTime(long timeInSeconds) {
+        long hours = TimeUnit.SECONDS.toHours(timeInSeconds);
+        long minutes = TimeUnit.SECONDS.toMinutes(timeInSeconds) % 60;
+        long seconds = timeInSeconds % 60;
+        return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        long currentTime = System.currentTimeMillis();
+        if (lastMovementTime != 0) {
+            long currentDuration = currentTime - lastMovementTime;
+            long currentDurationInSeconds = TimeUnit.MILLISECONDS.toSeconds(currentDuration);
+            if (currentDurationInSeconds > recordTime) {
+                recordTime = currentDurationInSeconds;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong("recordTime", recordTime);
+                editor.apply();
+            }
+        }
+        super.onTaskRemoved(rootIntent);
+        stopSelf();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used
+    }
+}
