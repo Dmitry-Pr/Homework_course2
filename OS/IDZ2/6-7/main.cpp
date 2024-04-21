57d5c68e6ca4d935814cfd293571bd31d5496cd2
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-04-21 15:15:42 +0300

    Completed 6-7 marks

---
+++ OS/IDZ2/6-7/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <semaphore.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <random>
+#include <fstream>
+#include <cstdlib>
+#include <sys/wait.h>
+#include <sys/mman.h>
+
+const int NUM_DEPARTMENTS = 3;
+int NUM_CUSTOMERS = 4;
+sem_t *departments[NUM_DEPARTMENTS];
+std::vector<pid_t> seller_pids;
+std::ofstream out;
+sem_t *customer_arrived[NUM_DEPARTMENTS];
+
+void handle_sigint(int sig) {
+    for (int i = 0; i < NUM_DEPARTMENTS; ++i) {
+        sem_destroy(departments[i]);
+        sem_destroy(customer_arrived[i]);
+    }
+    exit(0);
+}
+
+void seller(int dep_num) {
+    while (true) {
+        sem_wait(customer_arrived[dep_num]);  // Продавец ждет покупателя
+        std::cout << "Seller in department " << dep_num + 1 << " is busy" << std::endl;
+        if (out.is_open()) {
+            out << "Seller in department " << dep_num + 1 << " is busy" << std::endl;
+        }
+        sleep(1);  // Симулируем время, необходимое для обслуживания покупателя
+        std::cout << "Seller in department " << dep_num + 1 << " is free" << std::endl;
+        if (out.is_open()) {
+            out << "Seller in department " << dep_num + 1 << " is free" << std::endl;
+        }
+        sem_post(departments[dep_num]);
+    }
+}
+
+void customer(int i) {
+    std::cout << "Customer " << i << " has entered the store" << std::endl;
+    if (out.is_open()) {
+        out << "Customer " << i << " has entered the store" << std::endl;
+    }
+    srand(getpid());
+    auto repeat = rand() % NUM_DEPARTMENTS + 1;
+    for (int j = 0; j < repeat; ++j) {
+        auto dep_num = rand() % NUM_DEPARTMENTS;
+        sem_t *department = departments[dep_num];
+
+        std::cout << "Customer " << i << " is in queue in department " << dep_num + 1 << std::endl;
+        if (out.is_open()) {
+            out << "Customer " << i << " is in queue in department " << dep_num + 1 << std::endl;
+        }
+        sem_post(customer_arrived[dep_num]);  // Покупатель сообщает о своем приходе
+        sem_wait(department);
+        sleep(1);
+        std::cout << "Customer " << i << " is done shopping in department " << dep_num + 1 << std::endl;
+        if (out.is_open()) {
+            out << "Customer " << i << " is done shopping in department " << dep_num + 1 << std::endl;
+        }
+        sleep(1);
+    }
+    std::cout << "Customer " << i << " has left the store" << std::endl;
+    if (out.is_open()) {
+        out << "Customer " << i << " has left the store" << std::endl;
+    }
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " NUM_CUSTOMERS [OUTPUT_FILE]" << std::endl;
+        return 1;
+    }
+
+    NUM_CUSTOMERS = std::atoi(argv[1]);
+    if (argc == 3) {
+        out.open(argv[2]);
+        if (!out) {
+            std::cerr << "Failed to open output file " << argv[2] << std::endl;
+            return 1;
+        } else {
+            std::cout << "Output is also redirected to " << argv[2] << std::endl;
+        }
+    }
+
+    signal(SIGINT, handle_sigint);
+
+    // Создаем семафоры и процессы для каждого отдела
+    for (int i = 0; i < NUM_DEPARTMENTS; ++i) {
+        departments[i] = (sem_t*)mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        customer_arrived[i] = (sem_t*)mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        sem_init(departments[i], 1, 1);
+        sem_init(customer_arrived[i], 1, 0);
+        pid_t pid = fork();
+        if (pid == 0) {
+            seller(i);
+            return 0;
+        } else {
+            seller_pids.push_back(pid);  // Добавляем PID продавца в список
+        }
+    }
+
+    // Создаем процессы для каждого покупателя
+    for (int i = 1; i <= NUM_CUSTOMERS; ++i) {
+        if (fork() == 0) {
+            customer(i);
+            return 0;
+        }
+    }
+
+    // Ждем завершения всех процессов
+    for (int i = 0; i < NUM_CUSTOMERS; ++i) {
+        wait(NULL);
+    }
+    std::cout << "All customers have left the store" << std::endl;
+    if (out.is_open()) {
+        out << "All customers have left the store" << std::endl;
+    }
+    for (pid_t pid: seller_pids) {
+        kill(pid, SIGINT);
+    }
+    // Удаляем семафоры
+    for (int i = 0; i < NUM_DEPARTMENTS; ++i) {
+        sem_destroy(departments[i]);
+        sem_destroy(customer_arrived[i]);
+    }
+
+    return 0;
+}
