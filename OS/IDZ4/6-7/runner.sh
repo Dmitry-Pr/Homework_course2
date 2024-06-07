b4c3c042e88947f8feb05b90674da30d287eacb6
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-06-08 00:14:16 +0300

    Added IDZ4 OS completed 6-7 mark

---
+++ OS/IDZ4/6-7/runner.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+IP=$1
+PORT=$2
+NUM_CUSTOMERS=$3
+NUM_OUTPUTS=$4
+
+g++ -g shop.cpp -o shop
+g++ -g seller.cpp -o seller
+g++ -g customer.cpp -o customer
+g++ -g output.cpp -o output
+
+echo "Running shop on $IP:$PORT"
+./shop $IP $PORT >shopout.txt 2>&1 &
+SHOP_PID=$!
+
+echo "Running $NUM_OUTPUTS outputs"
+sleep 1
+
+for i in $(seq 1 $NUM_OUTPUTS); do
+./output $IP $PORT >"output$i.txt" 2>&1 &
+done
+
+# Перехватываем SIGINT и передаем его серверу
+trap "kill -SIGINT $SHOP_PID" SIGINT
+
+echo "Running 3 sellers"
+sleep 1
+
+for i in $(seq 1 3); do
+  ./seller $IP $PORT >"seller$i.txt" 2>&1 &
+done
+
+sleep 1
+
+echo "Running $NUM_CUSTOMERS customers"
+
+for i in $(seq 1 $NUM_CUSTOMERS); do
+  ./customer $IP $PORT >"customer$i.txt" 2>&1 &
+  sleep 1
+done
+
+
+wait $SHOP_PID
